103308374a5f1c25e14f0534fbdaaed90e0f6d56
Tvesha Shah <tveshashah13@gmail.com>
2024-04-07 11:24:55 -0400

    BFS MVP

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/BFS.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/BFS.java
@@ -16,11 +16,11 @@ public class BFS implements MazeSolver {
     public static Position goalPos; 
     public static Direction dir;
 
-    public static Queue<Position> queue = new LinkedList<Position>();
-    public static ArrayList<Position> explored = new ArrayList<>(); 
-    public static Queue<Direction> direction = new LinkedList<Direction>();
+    public static Queue<Node> queue = new LinkedList<Node>();
+    public static ArrayList<Node> explored = new ArrayList<Node>(); 
+   //public static Queue<Direction> direction = new LinkedList<Direction>();
 
-    public static Map<Position, Position> parentTracker = new HashMap<>(); 
+    public static Map<Position, Position> parentTracker = new HashMap<>(); //dictionary tracking new nodes and the parent
 
     @Override
     public Path solve(Maze maze) {
@@ -35,51 +35,53 @@ public class BFS implements MazeSolver {
         //current positions and goal position 
         currentPos = maze.getStart();      
         goalPos = maze.getEnd();
-        
-        //start direction - always facing right 
+        Direction dir = Direction.RIGHT; //starting direction, always right 
         
         //start point storage 
-        explored.add(currentPos);
-        queue.offer(currentPos); 
+        explored.add(new Node(currentPos, dir));
+        queue.offer(new Node(currentPos, dir)); 
         parentTracker.put(currentPos, null);
-        direction.offer(Direction.RIGHT);
+        //direction.offer(Direction.RIGHT);
         
         while (!(queue.isEmpty())) {
-            currentPos = queue.poll();
-            dir = direction.poll();
+            Node currentNode = queue.poll();
+            dir = currentNode.getDirection();
+            currentPos = currentNode.getPosition();
 
-            if (currentPos.equals(goalPos)) {
+            if (currentPos.equals(goalPos)) {//exit start node and goal node same 
                 return true; 
             }
-            //create list of three nodes right left and front of current
-            //if never searched and no wall before add to queue, if searched do not add to queue
-            ArrayList<Position> nodes = around(maze); //list of nodes around current 
 
-            for (Position node:nodes){
-                if(!explored.contains(node)){
-                    queue.offer(node);
-                    explored.add(node);//tracks all explored nodes 
-                    parentTracker.put(node,currentPos); //tracks new node and parent
+            ArrayList<Node> nodes = around(maze); //list of nodes around current 
 
+            for (Node node:nodes){//loop through all nodes around 
+                if(!explored.contains(node)){//if not already explored 
+                    queue.offer(node);//add to queue 
+                    explored.add(node);//tracks in explored 
+                    parentTracker.put(node.getPosition(),currentPos); //tracks new node and parent
+
+                    if (node.getPosition() == goalPos){//checks if goal position reached with new addition 
+                        return true;
+                    }
                 }
             }
         }
         return true; 
     }
 
-    public static ArrayList<Position> around(Maze maze){
+    public static ArrayList<Node> around(Maze maze){
         //check nodes left and right and if they are not walls, add to queue 
-        ArrayList<Position> nodes = new ArrayList<>();
+        ArrayList<Node> nodes = new ArrayList<>();
 
         if(!maze.isWall(currentPos.move(dir))){
-            nodes.add(currentPos.move(dir)); 
+            nodes.add(new Node(currentPos.move(dir), dir)); 
         }
         if (!maze.isWall(currentPos.move(dir.turnLeft()))){
-            nodes.add(currentPos.move(dir)); 
+            nodes.add(new Node(currentPos.move(dir), dir.turnLeft())); 
         }
 
         if (!maze.isWall(currentPos.move(dir.turnRight()))){
-            nodes.add(currentPos.move(dir)); 
+            nodes.add(new Node(currentPos.move(dir), dir.turnRight())); 
         }
 
         return nodes; 
@@ -87,13 +89,24 @@ public class BFS implements MazeSolver {
 
     public Path path() {
         Path path = new Path();
-
         return path; 
     }
+}
 
-    //create a state class to store direction, position and output
+        //go through parent tracker tree from goalnode position to start node and get order of nodes (from start to end)
+        //from this list loop through the explored nodes from the beggining, set a local direction variable 
+        //intia;l direction always right 
+        //if the direction is same as previous/local direction variable add F to the string
+        //if different add LF or RF (based on the direction change - if it's going right and then it goes up that means LF)
+        //return path 
+        
+ //create a state class to store direction, position and output
+    //so explored and queue will be position, direction
+    //parentTracker would be position new, position parent, output/path (to get from parent to new) 
+
+    //or just change this for explored and queue - like add path for them
+    //then for output
+
     //OUTPUT: (maybe - like F, L R --> Like to reach from current not to this node do FFR)
     //so all the nodes will states be updated like that in the queues
     //for the final path go through the tree and collect all the paths, then flip the string to get the right order 
-
-}
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Node.java
@@ -0,0 +1,26 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Node {
+
+    private Position position;
+    private Direction direction;
+    //private Path path; 
+
+    public Node(Position position, Direction direction) {
+        this.position = position;
+        this.direction = direction;
+        //this.path = path;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+    
+    /*public Path getPath() {//path from previous node to new node
+        return path;
+    }*/
+}
