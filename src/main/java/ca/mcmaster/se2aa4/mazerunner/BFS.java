2ea4308170c9981a2c614f82667157c9b1967d43
Tvesha Shah <tveshashah13@gmail.com>
2024-04-06 21:14:06 -0400

    bfs mvp

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/BFS.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/BFS.java
@@ -1,7 +1,9 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,6 +11,15 @@ import org.apache.logging.log4j.Logger;
 
 public class BFS implements MazeSolver {
     private static final Logger logger = LogManager.getLogger();
+
+    public static Position currentPos;
+    public static Position goalPos; 
+    public static Direction dir;
+
+    public static Queue<Position> queue = new LinkedList<Position>();
+    public static ArrayList<Position> explored = new ArrayList<>(); 
+
+    public static Map<Position, Position> parentTracker = new HashMap<>(); 
 
     @Override
     public Path solve(Maze maze) {
@@ -27,9 +38,7 @@ public class BFS implements MazeSolver {
         //start direction - always facing right 
         Direction dir = Direction.RIGHT;
 
-        Queue<Position> queue = new LinkedList<Position>();
-        ArrayList<Position> explored = new ArrayList<>(); 
-
+        explored.add(currentPos);
         queue.offer(currentPos); 
         
         while (!(queue.isEmpty())) {
@@ -38,9 +47,38 @@ public class BFS implements MazeSolver {
             if (currentPos.equals(goalPos)) {
                 return true; 
             }
-            
+            add(maze); //adds nodes to queue
+
+            for (Position node:queue){
+                if(!explored.contains(node)){
+                    explored.add(node);//tracks all explored nodes 
+
+                    parentTracker.put(node,currentPos); //tracks new node and parent
+
+                }
+            }
+
+
+
         }
         return true; 
+    }
+
+    public static void add(Maze maze){
+        //check nodes left and right and if they are not walls, add to queue 
+        
+        if(!maze.isWall(currentPos.move(dir))){
+            queue.offer(currentPos.move(dir)); 
+        }
+
+        if (!maze.isWall(currentPos.move(dir.turnLeft()))){
+            queue.offer(currentPos.move(dir)); 
+        }
+
+        if (!maze.isWall(currentPos.move(dir.turnRight()))){
+            queue.offer(currentPos.move(dir)); 
+        }
+
     }
 
     public Path path() {
